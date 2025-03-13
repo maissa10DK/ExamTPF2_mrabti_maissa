d92f4c0069e3158eb3652f8a64e35e967b4f4c88
maissa mrabti <maissamrabti2001@gmail.com>
2025-03-13 10:28:38 +0100

    dernier commit apres correc

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ def somme(T):
    return S
 
 Data=[1,3,5]
- HEAD
+
 
 
 
@@ -16,14 +16,9 @@ print("la somme est:",som)
 
 
 
- projectf2
 if Data:
   print("la somme est:",sum(Data))
   print("le min est:",min(Data))
   print("le max est:",max(Data)
 else:
   print("le dossier est vide"
- HEAD
-
-
- projectf2
